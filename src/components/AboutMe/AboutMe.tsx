ff77983ee6a77195e7f979806583a594482af782
Myroslav Toloshnyi <dfgshte@gmail.com>
2025-04-28 13:24:55 +0300

    Migrate to Next

---
+++ src/components/AboutMe/AboutMe.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+
+import Container from "@/components/Container/Container";
+import Heading from "@/components/Heading/Heading";
+
+import styles from "./AboutMe.module.scss";
+
+const startedProgramming = new Date("2019-11-27").getFullYear();
+const currentYear = new Date().getFullYear();
+const yearsOfDevelopment = currentYear - startedProgramming;
+
+const AboutMe: React.FC = () => {
+  return (
+    <Container id="about" isBig={true} className={styles.outer}>
+      <div className={styles.dots}>
+        <img src="img/about-me/dots.svg" alt="dots-svg" />
+      </div>
+
+      <div className={styles.script}>
+        <img src="img/preview/script.svg" alt="script-svg" />
+      </div>
+
+      <div className={styles.span}>
+        <img src="img/preview/span.svg" alt="span-svg" />
+      </div>
+
+      <div className={styles.mirosh}>
+        <img src="img/about-me/mirosh.png" alt="mirosh-svg" />
+      </div>
+
+      <div className={styles.elips}>
+        <img src="img/preview/elips.svg" alt="elips-svg" />
+      </div>
+
+      <div className={styles.elipsSmall}>
+        <img src="img/preview/elips-small.svg" alt="elips-small-svg" />
+      </div>
+
+      <Container className={styles.inner}>
+        <div className={styles.text}>
+          <div className={styles.inner}>
+            <Heading text="Few Words" level={3} />
+            <Heading text="About Me" level={1} />
+            <table className={styles.list}>
+              <thead>
+                <tr>
+                  <td>Experience</td>
+                  <td>{yearsOfDevelopment}+ years</td>
+                </tr>
+              </thead>
+              <thead>
+                <tr>
+                  <td>Degree</td>
+                  <td>Master of Networking and Web Technologies</td>
+                </tr>
+              </thead>
+            </table>
+            <p>
+              Experienced Software Engineer ({yearsOfDevelopment}+ years of
+              experience). Freestyler, Leader, Mentor and Achiever with deep
+              understanding of sustainability and architecture shaping.
+            </p>
+          </div>
+        </div>
+      </Container>
+    </Container>
+  );
+};
+
+export default AboutMe;
---
+++ src/components/Nav/Nav.tsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+
+import Link from "next/link";
+import { Link as LinkType } from "../Header/Header";
+
+import styles from "./Nav.module.scss";
+
+type NavPropsTypes = {
+  navLinks: Array<LinkType>;
+  opened: boolean;
+};
+
+const Nav: React.FC<NavPropsTypes> = (props): React.JSX.Element => {
+  const { navLinks, opened } = props;
+
+  const [activeIndex, setActiveIndex] = useState<number>();
+
+  const toggleLinksClass = (index: number) => {
+    setActiveIndex(index);
+  };
+
+  const classNames = [styles.nav];
+  opened && classNames.push(styles._opened);
+
+  return (
+    <nav className={classNames.join(" ")}>
+      <ul className={styles.list}>
+        {navLinks.map((link, index) => {
+          const delayOnTransiotion = {
+            transitionDuration: `${(navLinks.length + Number(index) + 1) / 6}s`,
+          };
+          const classNames = [styles.link];
+          if (activeIndex === index) classNames.push(styles._active);
+
+          return (
+            <li
+              key={index.toString()}
+              className={styles.item}
+              style={delayOnTransiotion}
+            >
+              <Link
+                className={classNames.join(" ")}
+                href={link.href}
+                key={index.toString()}
+                onClick={() => {
+                  toggleLinksClass(index);
+                }}
+              >
+                {link.text}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
